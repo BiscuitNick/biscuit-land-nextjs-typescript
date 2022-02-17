9d9d682e21d96525e8b1a7293c03503e7d37aced
BiscuitNick <nick@biscuit.land>
2022-02-17 01:37:01 -0800

    3.5.12

--- src/BiscuitLibrary.d.ts
+++ src/BiscuitLibrary.d.ts
@@ -1,3 +1,6 @@
 declare module "@biscuitnick/biscuit-library";
 declare module "ColorPickers";
 declare module "NumberInput";
+declare module "useEventListener";
+declare module "useInterval";
+declare module "useWindowSize";
