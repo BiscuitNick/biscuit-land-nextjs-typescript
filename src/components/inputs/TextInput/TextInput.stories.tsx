7c2b90a4ccd48605011a3e81c86b5385784ce530
BiscuitNick <nick@biscuit.land>
2022-02-17 01:10:11 -0800

    3.5.7

--- src/components/inputs/TextInput/TextInput.stories.tsx
+++ src/components/inputs/TextInput/TextInput.stories.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
-import TextInput from "./TextInput";
+import TextInput from "./";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
--- src/components/inputs/ToggleSwitch/ToggleSwitch.stories.tsx
+++ src/components/inputs/ToggleSwitch/ToggleSwitch.stories.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
-import ToggleSwitch from "./ToggleSwitch";
+import ToggleSwitch from "./";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
