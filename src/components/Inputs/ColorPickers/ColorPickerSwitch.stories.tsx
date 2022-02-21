0eccafff396e7e84c8c770aff4228d35f2f7497f
BiscuitNick <nick@biscuit.land>
2022-02-21 15:27:36 -0800

    3.8.6

--- src/components/Inputs/ColorPickers/ColorPickerSwitch.stories.tsx
+++ src/components/Inputs/ColorPickers/ColorPickerSwitch.stories.tsx
@@ -5,7 +5,7 @@ import "../../../../styles/globals.css";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: "Biscuit-Library/Inputs/ColorPickers/ColorPickerSwitch",
+  title: "Biscuit-Library/Inputs/ColorPickerSwitch",
   component: ColorPickerSwitch,
   argTypes: {
     value: {
